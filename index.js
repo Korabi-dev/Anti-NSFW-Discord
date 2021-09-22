e363b54ae255c42af9b70b7279aaeb30f444e2bf
Korab Arifi <korabarifi53@gmail.com>
2021-09-22 03:55:42 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const axios = require('axios')
+const tf = require('@tensorflow/tfjs-node')
+const nsfw = require('nsfwjs')
+const discord = require("discord.js")
+require("dotenv").config()
+const client = new discord.Client({intents: 32767})
+async function isnsfw(url) {
+    let r = false;
+  const pic = await axios.get(url, {
+    responseType: 'arraybuffer',
+  })
+  const model = await nsfw.load()
+  const image = await tf.node.decodeImage(pic.data,3)
+  const predictions = await model.classify(image)
+  image.dispose()
+  predictions.map((pr) => {
+    pr.probability = Math.round(pr.probability * 100)
+    if(pr.className == "Hentai" && pr.probability > 30) r = true
+    if(pr.className == "Porn" && pr.probability > 30) r = true
+  })
+  return r
+}
+
+client.on("messageCreate", async(message) => {
+    if(!message.attachments || message.author.bot == true || message.channel.nsfw == true) return;
+message.attachments.map(async(attachment) => {
+    if(attachment.contentType.toLowerCase().startsWith("image")){
+        if(await isnsfw(attachment.url) == true){
+            await message.delete()
+            message.channel.send({embeds: [new discord.MessageEmbed().setTitle("No nsfw here").setDescription(`${message.author} you can NOT send nsfw here.`).setColor("DARK_RED")]})
+        } 
+    }
+})
+})
+
+client.on("ready", () => {
+    console.log(`${client.user.tag} is now online.`)
+})
+
+client.login(process.env.token)
